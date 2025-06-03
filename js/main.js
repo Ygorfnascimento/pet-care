e656411aaf293b34a721f45a543c44eeb78bbc5a
ygorsenai <ygorferreiradonascimentosilva@gmail.com>
2025-06-03 09:08:28 -0300

    Adicionando a seção dúvidas + script JS

--- js/main.js
+++ js/main.js
@@ -1,31 +1,15 @@
-/* Seleciona os elementos com a classe "duvida" */
+/* Seleione todos os elementos com a classe "duvida" */
+const elementosDuvida = document.querySelectorAll(".duvida");
 
-document.addEventListener('DOMContentLoaded', function() {
-    
-    const duvidas = document.querySelectorAll('.duvida');
-    
-    duvidas.forEach(duvida => {
-        const seta = duvida.querySelector('img');
-        const resposta = duvida.querySelector('p');
-        
-        resposta.style.display = 'none';
-        
-        seta.addEventListener('click', function() {
-            
-            duvidas.forEach(otherDuvida => {
-                if (otherDuvida !== duvida) {
-                    otherDuvida.querySelector('p').style.display = 'none';
-                    otherDuvida.querySelector('img').src = '/img/arrow-down.svg'; 
-                }
-            });
-            
-            if (resposta.style.display === 'none') {
-                resposta.style.display = 'block';
-                seta.src = '/img/arrow-down.svg'; 
-            } else {
-                resposta.style.display = 'none';
-                seta.src = '/img/arrow-down.svg'; 
+elementosDuvida.forEach(function (duvida) {
+  duvida.addEventListener("click", function () {
+        /* Fecha as outras duvidas */
+        elementosDuvida.forEach(function (outraDuvida) {
+            if (outraDuvida !== duvida) {
+              outraDuvida.classList.remove("ativa");
             }
         });
-    });
+        /* Abre a duvida */
+    duvida.classList.toggle("ativa");
+  });
 });
